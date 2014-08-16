9af7eab028933dec25b4d3c9ec173381093782de
Designmoves <github@designmoves.nl>
2014-08-16 22:04:54 +0200

    Updated test setup

--- test/Bootstrap.php
+++ test/Bootstrap.php
@@ -1,4 +1,33 @@
 <?php
+/**
+ * Copyright (c) 2014, Designmoves http://www.designmoves.nl
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * * Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * * Neither the name of Designmoves nor the names of its
+ *   contributors may be used to endorse or promote products derived from
+ *   this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 
 namespace DesignmovesApplicationTest;
 
@@ -6,10 +35,6 @@ use Zend\Loader\AutoloaderFactory;
 use Zend\Mvc\Service\ServiceManagerConfig;
 use Zend\ServiceManager\ServiceManager;
 use RuntimeException;
-
-date_default_timezone_set('Europe/Amsterdam');
-error_reporting(-1);
-chdir(__DIR__);
 
 /**
  * Test bootstrap, for setting up autoloading
@@ -106,9 +131,28 @@ class Bootstrap
             }
             $previousDir = $dir;
         }
+
         return $dir . '/' . $path;
     }
 }
 
+/**
+ * Travis CI specific code
+ */
+if (0 === strpos(__DIR__, '/home/travis/build/')) {
+    $vendorDir = __DIR__ . '/../vendor';
+
+    if (is_file($file = $vendorDir . '/autoload.php')) {
+        require_once $file;
+    } elseif (is_file($file = './vendor/autoload.php')) {
+        require_once $file;
+    } else {
+        throw new RuntimeException('Composer autoload file not found');
+    }
+
+    return;
+}
+
+chdir(__DIR__);
 Bootstrap::init();
 Bootstrap::chroot();
